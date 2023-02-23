8f2d52e4c0ad2c73d813caf9ea5fe059f41b52e4
Zohan Subhash <zohan.subhash@gmail.com>
2023-02-23 05:33:15 +0000

    Restructure to object inputs + Change verbose to callback

--- singleGeneration.js
+++ singleGeneration.js
@@ -1,6 +1,11 @@
-function singleGeneration(p = 0.6, starting = 2000, offspring = 2) {
+function singleGeneration(options) {
+    const {
+        p = 0.6,
+        starting = 2000,
+        offspring = 2
+    } = options
     const multiGeneration = require('./multiGeneration')
-    return multiGeneration(p, starting, offspring, 1, false, false)
+    return multiGeneration({ p, starting, offspring, generations: 1, limitPopulation: false, verbose: false })
 }
 
 module.exports = singleGeneration
